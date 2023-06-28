c4dd352ae4f902371c3a6f785526f1a579c0bce6
ziogenorwekh <ziogenorwekh@gmail.com>
2023-06-28 17:26:48 +0900

    test

--- src/App.test.js
+++ src/App.test.js
@@ -2,19 +2,17 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import axios from "axios";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
-// test('API 연결 확인', async () => {
-//   try {
-//     const response = await axios.get('http://172.17.0.1:9000/health-check');
-//
-//     expect(response.status).toBe(200);
-//     expect(response.data).toBeDefined();
-//   } catch (error) {
-//     expect(error).toBeNull();
-//   }
-// });
+test('test api',()=>{
+    let response = '';
+    const resp = axios.get(`${process.env.REACT_APP_API}/health-check`)
+        .then(resp2=>{
+            response = resp2.data;
+        })
+    expect(response).toEqual('connected');
+})
