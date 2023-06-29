eb7be7e1d3abdfad9e327ea451b45c92b13f6d67
ziogenorwekh <ziogenorwekh@gmail.com>
2023-06-29 18:17:37 +0900

    A

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import axios from "axios";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
 test('test api',()=>{
     let response = '';
